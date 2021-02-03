3127f647e8e586b081c6d8573aa79d54642b8513
Justin Dorfman <justindorfman@gmail.com>
2021-02-03 12:57:26 -0800

    Fix Typo
    Signed-off-by: Justin Dorfman <justindorfman@gmail.com>

--- environments/build/1_deploy_curiefense.sh
+++ environments/build/1_deploy_curiefense.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install -y git docker.io docker-compose libpq-dev python3-dev gcc python3-psycopg2
+apt-get install -y git docker.io docker-compose libpq-dev python3-dev gcc python3-psycopg2 vim
 
 git clone https://github.com/curiefense/curiefense.git
 cd curiefense/deploy/compose/
