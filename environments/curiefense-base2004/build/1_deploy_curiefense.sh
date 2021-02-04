238f1b34fd6a2dbb268bdb97eb4c32ad201c41ae
Justin Dorfman <justindorfman@gmail.com>
2021-02-04 14:40:16 -0800

    Replace build argument with pull for Docker
    Signed-off-by: Justin Dorfman <justindorfman@gmail.com>

--- environments/curiefense-base2004/build/1_deploy_curiefense.sh
+++ environments/curiefense-base2004/build/1_deploy_curiefense.sh
@@ -4,4 +4,4 @@ apt-get install -y git docker.io docker-compose libpq-dev python3-dev gcc python
 
 git clone https://github.com/curiefense/curiefense.git
 cd curiefense/deploy/compose/
-docker-compose build
+docker-compose pull
