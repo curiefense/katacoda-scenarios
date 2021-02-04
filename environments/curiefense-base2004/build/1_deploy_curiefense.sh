b707cd2709f88c1145e31762db0e754fff7354d6
Justin Dorfman <justindorfman@gmail.com>
2021-02-04 14:48:32 -0800

    Add build argument
    Signed-off-by: Justin Dorfman <justindorfman@gmail.com>

--- environments/curiefense-base2004/build/1_deploy_curiefense.sh
+++ environments/curiefense-base2004/build/1_deploy_curiefense.sh
@@ -5,3 +5,4 @@ apt-get install -y git docker.io docker-compose libpq-dev python3-dev gcc python
 git clone https://github.com/curiefense/curiefense.git
 cd curiefense/deploy/compose/
 docker-compose pull
+docker-compose build
