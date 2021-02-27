bef145c7f7af30b766bcf57d168703acb71014ce
Justin Dorfman <justindorfman@gmail.com>
2021-02-27 12:33:46 -0800

    Update background
    Signed-off-by: Justin Dorfman <justindorfman@gmail.com>

--- api-basics/background.sh
+++ api-basics/background.sh
@@ -3,12 +3,7 @@
 cd /root/curiefense/deploy/compose
 docker-compose up -d
 
-sleep 5
-
 echo "done" | sudo tee /root/katacoda-finished
-
 echo "Everything ready... Finalise the deployment"
-
 /usr/local/bin/deploy.sh
-
 echo "done" | sudo tee /root/katacoda-background-finished
