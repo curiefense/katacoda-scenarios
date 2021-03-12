0bc2db18ffa1ea268ae6dc28e46f41f741ba0368
Justin Dorfman <justindorfman@gmail.com>
2021-03-12 15:44:45 -0800

    Tag rules initial commit
    Signed-off-by: Justin Dorfman <justindorfman@gmail.com>

---
+++ tag-rules/assets/wait.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+show_progress()
+{
+  echo -n "⏳ Initializing"
+  local -r pid="${1}"
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp
+  while true; do 
+    sudo grep -i "done" /root/katacoda-finished &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "✅ Initialized"
+  echo -n "⚙️ Configuring"
+  while true; do 
+    sudo grep -i "done" /root/katacoda-background-finished &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "🎉 Configured!  Now Start Learning! 👩‍💻"
+}
+
+show_progress
---
+++ tag-rules/step1.md
@@ -0,0 +1,23 @@
+## Create a Tag Rules List
+
+Open the UI management console by clicking the "[Management UI](https://[[HOST_SUBDOMAIN]]-30080-[[KATACODA_HOST]].environments.katacoda.com)" tab.
+
+Start by creating a new Tag Rules List by selecting the "+" button at the top:
+
+![New Tag Rule](./assets/tag-rules-add.png)
+
+Next, in the Tags text box on the left, enter the value `trusted`.
+
+![New Tag: Trusted](./assets/new-tag-trusted.jpg)
+
+Then, at the top of the (currently empty) list to the right, add a new entry by selecting the `Create New Section` button. An empty rule will appear.
+
+In the left pulldown, select Header. Enter `foo` for its name, and `bar` for its value. Then select "Add".
+
+![New Tag: Add](./assets/foo-bar-add.jpg)
+
+We have created a simple tag rule. Every request that contains a header named `foo` which matches the regex (PCRE) `bar` will receive a tag of `trusted`. 
+
+Now save the new configuration:
+
+![New Tag: Save](./assets/save-tag-rule.jpg)
